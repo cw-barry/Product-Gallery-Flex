39c67135b646082aa2a378908a6aa3f356d115a6
cw-barry <barry@clarusway.com>
2023-10-05 16:26:32 -0400

    ft: add to cart button added

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const renderProducts = async () => {
   const data = await getData();
   const html = data.map(
     ({ image, title, description, price }) => `<div class="product-card">
+    <div>
     <img src="${image}" alt="${title}" />
     <h2>${title}</h2>
     <p>${description}</p>
     <span>$${price}</span>
+    </div>
+    <div>
+    <button class="add-to-cart">Add to Cart</button>
+    </div>
   </div>`
   );
   const div = document.querySelector('.gallery');
